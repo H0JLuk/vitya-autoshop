31fedb0ee225fab4f6e39184284be89e77077090
Sergey Smolkov <damager4425@gmail.com>
2022-04-26 15:28:24 +0300

    feat: add templates

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,24 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function (req, res, next) {
+  res.render('index');
+});
+
+router.get('/contact-us', (req, res, next) => {
+  res.render('contact-us');
+});
+
+router.get('/articles', (req, res, next) => {
+  res.render('articles');
+});
+
+router.get('/articles/:id', (req, res, next) => {
+  res.render('article');
+});
+
+router.get('/about-us', (req, res, next) => {
+  res.render('about-us');
 });
 
 module.exports = router;
